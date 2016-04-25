84b1d787734885c713f499f6741c97672f4c63ed
aligarh09 <aligarh09@mail.ru>
2016-04-25 05:39:14 +0300

    Almost final version is ready

--- Web_site_client/a_olymptrade.cpp
+++ Web_site_client/a_olymptrade.cpp
@@ -1,5 +1,5 @@
 //TODO::investing.com signals handler
-//g++ a_olymptrade.cpp ParseOlymptradeJSON.cpp WinpercCommander.cpp WsClientCommander.cpp CurlOlymptradeActions.cpp json11/json11.cpp -std=c++11 -lcurl
+//g++ a_olymptrade.cpp ParseOlymptradeJSON.cpp OlyClientDealService.cpp WinpercCommander.cpp WsClientCommander.cpp CurlOlymptradeActions.cpp json11/json11.cpp -std=c++11 -lcurl
 
 #include <cstdio>
 #include <cstdlib>
@@ -17,6 +17,7 @@
 #include "WinpercCommander.h"
 #include "WsClientCommander.h"
 #include "CurlOlymptradeActions.h"
+#include "OlyClientDealService.h"
 #include "ParseOlymptradeJSON.h"
 
 //подключаем curl
@@ -24,38 +25,90 @@
 #pragma comment(lib,"curllib.lib");
 
 const int ASSETS_AMOUNT = 18;
-/*
-typedef struct BET_PROPERTIES
-{
-  long msgtyp;
-  int asset;
-  std::uint64_t id; 
-  std::uint32_t sum;
-  std::time_t timeframe;
-  bool direction;
-} bet_properties_t;
 
-typedef struct BET_RESPONSE
+size_t recieved_data_process(char *response_data, size_t size, size_t nmemb, void *user_func_args);
+void wait_time_left_before_next_update();
+
+int main()
 {
-  long msgtyp;
-  std::uint64_t id; 
-  bool result;
-} bet_response_t;
-*/
+  std::string login;
+  std::string password;
+  bool playing_real = 0;
+  std::cout <<"Enter user email: ";     getline(std::cin, login);
+  std::cout <<"Enter user password: ";  getline(std::cin, password);
+  std::cout <<"Will we play real? (1/0): ";  std::cin >> playing_real;
+  std::cout << std::endl;
+
+  OlyClientDealService deals_service;
+  CurlOlymptradeActions curl_connection;
+
+  curl_connection.log_into_platform(login, password, recieved_data_process, &deals_service);
+
+  if(!playing_real)
+    curl_connection.switch_to_demo();
+
+  wait_time_left_before_next_update();
+
+  std::vector<deals_namespace::NewBet> bet_props;
+
+  while(1)
+  {
+    deals_service.service_deal_status();
+    
+    bet_props = deals_service.get_new_bets();
+
+    printf("Bet amount:: %d\n\n\n\n", bet_props.size());
+
+    if(bet_props.size())
+      printf("%d %d %d %d %d\n\n", bet_props[0].bet_id, bet_props[0].asset, bet_props[0].direction, bet_props[0].deal_amount, bet_props[0].timeframe);
+
+    curl_connection.send_requests(bet_props);
+    
+    wait_time_left_before_next_update();
+  }
+
+  return 0;  
+}
+
+void wait_time_left_before_next_update()
+{
+    unsigned int sec_in_nano = 1000000000;
+
+    unsigned int update_sec_part = 700000000;
+
+    struct timespec spec;
+    long current_second_nano_part;
+
+    clock_gettime(CLOCK_REALTIME, &spec);
+
+    current_second_nano_part = spec.tv_nsec; // Convert nanoseconds to milliseconds
+
+    if(current_second_nano_part > update_sec_part)
+    {
+      usleep((sec_in_nano + update_sec_part - current_second_nano_part) / 1000);
+    }
+    else
+      usleep((update_sec_part - current_second_nano_part) / 1000);
+}
+
 size_t recieved_data_process(char *response_data, size_t size, size_t nmemb, void *user_func_args)
 {
   static int times_called = 0;
   static WsClientCommander commander;
   static WinpercCommander  winperc_commander;
+  static OlyClientDealService* deals_service = (OlyClientDealService*)user_func_args;
+  static ParseOlymptradeJSON parse_bet_data;
   static ParseOlymptradeJSON parse_data_even;
   static ParseOlymptradeJSON parse_data_odd;
-  static ParseOlymptradeJSON parse_new_deals_data[ASSETS_AMOUNT];
 
   std::string input(response_data, size * nmemb);
   
   if(!times_called)
   {
     times_called++;
+
+    deals_service -> set_deals_status_json_handler(&parse_bet_data);
+
     parse_data_odd.parse_login_json(input.c_str());                //counting from one, parse login response;
     
     if(!parse_data_odd.get_login_result())
@@ -71,9 +124,9 @@ size_t recieved_data_process(char *response_data, size_t size, size_t nmemb, voi
   {
     std::size_t found = input.find("\"content\"");
 
-    if(found != std::string::npos)                               //bet_data
+    if(found != std::string::npos)                                    //bet_data
     {
-
+      parse_bet_data.parse_bet_response_json(input.c_str());
     }
     else
     {
@@ -88,12 +141,14 @@ size_t recieved_data_process(char *response_data, size_t size, size_t nmemb, voi
           parse_data_odd.parse_update_json(input.c_str());
           commander.process_current_status(parse_data_odd);
           winperc_commander.update(parse_data_odd);
+          deals_service -> update_deals(&parse_data_odd);
         }
         else                                                          //even
         {
           parse_data_even.parse_update_json(input.c_str());
           commander.process_current_status(parse_data_even);
           winperc_commander.update(parse_data_even);
+          deals_service -> update_deals(&parse_data_even);
         }
       }
     }
@@ -101,68 +156,3 @@ size_t recieved_data_process(char *response_data, size_t size, size_t nmemb, voi
 
   return size * nmemb;
 }
-
-int main()
-{
-  std::string login;
-  std::string password;
-  std::cout <<"Enter user email: ";     getline(std::cin, login);
-  std::cout <<"Enter user password: ";  getline(std::cin, password);
-  std::cout << std::endl;
-
-  CurlOlymptradeActions curl_connection;
-
-  std::vector<bet_properties_t> ololo;
-
-  curl_connection.log_into_platform(login, password, recieved_data_process);
-
-  while(1)
-  {
-    curl_connection.send_requests(ololo);
-    sleep(1);
-  }
-
- /* while(1)
-  {
-    get_bet_data_from_neuro();
-    add_bet_to_curl_handle();
-    add_update_status_to_curl_handle();
-    curl_multi_perform();
-    compose_new_data_struct();
-    send_new_data_to_neuro();
-    check_assets_conditions();
-    if_new_day_then_set_new_connection();
-    wait_time_left_before_next_update();
-  }
-*/
-/*
-  bet_properties_t bet;
-  bet.duration = 60;
-  bet.amount = 30;
-  bet.pair = "EURUSD_OTC";
-  bet.direction = "down";
-
-  bet_properties_t bet2;
-  bet2.duration = 60;
-  bet2.amount = 30;
-  bet2.pair = "AUDUSD_OTC";
-  bet2.direction = "down";
-
-
-  curl_connection.switch_to_demo();
-
-
-  std::vector<bet_properties_t> vec;
-
-  vec.push_back(bet);
-
-  vec.push_back(bet2);
-
-  curl_connection.send_requests(vec);
-*/
-  return 0;  
-}
-
-/*
-run client; update every sec -> update; (at the end of the second, when all neurons(theoretically) have already process data and now handle a result)
-*/
